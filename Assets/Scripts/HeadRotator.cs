3708a3629e694dcab633101279dae13b82ba7a1f
Regoradin <oliver@theabates.com>
2016-08-24 20:11:51 -0500

    Head Look Bug
    Fixed that stupid thing!

--- Assets/Scripts/HeadRotator.cs
+++ Assets/Scripts/HeadRotator.cs
@@ -20,9 +20,15 @@ public class HeadRotator : MonoBehaviour {
 	void Update () {
 
 		transform.Rotate (-Input.GetAxis("Mouse Y")* looksensitivity, Input.GetAxis("Mouse X") * looksensitivity, 0.0f, Space.Self);
-		yRotation = Mathf.Clamp (transform.eulerAngles.x, minY, maxY);
-//		Debug.Log (yRotation);
-		transform.eulerAngles = new Vector3 (yRotation, transform.eulerAngles.y, 0f);
 
+		float a = transform.eulerAngles.x;
+		if (transform.eulerAngles.x > maxY + 10)
+			a = transform.eulerAngles.x - 360;
+
+		yRotation = Mathf.Clamp (a, minY, maxY);
+
+		Debug.Log("Old: " + a);
+		Debug.Log ("New: " + yRotation);
+		transform.eulerAngles = new Vector3 (yRotation, transform.eulerAngles.y, 0f);
 	}
 }
