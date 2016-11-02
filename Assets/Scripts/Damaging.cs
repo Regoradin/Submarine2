a080f6ba53e8f9330b5a26f92884f6de1443dbd6
Regoradin <oliver@theabates.com>
2016-11-02 17:28:59 -0500

    Basic Damage System
    HealthManager script to keep track of health and take damage, damaging
    script to inflict damage, only does damage if relative velocity
    difference is above a set amount.

---
+++ Assets/Scripts/Damaging.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damaging : MonoBehaviour {
+
+	public int damage;
+
+	private float minSpeed = 0f;
+
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Destroy(gameObject);
+		if(collision.relativeVelocity.magnitude >= minSpeed)
+		{
+			collision.collider.gameObject.GetComponent<HealthManager>().Health -= damage;
+			Debug.Log(collision.collider.gameObject.GetComponent<HealthManager>().Health);
+		}
+
+	}
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+	public int maxHealth;
+	private int health;
+	public int Health
+	{
+		get
+		{
+			return health;
+		}
+		set
+		{
+			health = value;
+			if(health > maxHealth)
+			{
+				health = maxHealth;
+			}
+			if (health < 0)
+			{
+				health = 0;
+			}
+		}
+	}
+	
+	private Color defaultColor;
+
+	private Renderer rend;
+	
+	void Start()
+	{
+		health = maxHealth;
+		rend = GetComponent<Renderer>();
+		defaultColor = rend.material.color;
+	}
+
+	void Update () {
+
+		if(health <= maxHealth / 2)
+		{
+			rend.material.color = new Color(1, 0, 0, 1);
+		}
+
+		if(health == 0)
+		{
+			Destroy(gameObject);
+		}
+
+		else
+		{
+			rend.material.color = defaultColor;
+		}
+
+	}
+}
