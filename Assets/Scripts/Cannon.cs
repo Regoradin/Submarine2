d6d21aea8084969c819e7fef0b5ca22a94c6b120
Regoradin <oliver@theabates.com>
2016-12-23 11:47:48 -0600

    Cannon!
    The first attempt at implementing a cannon. Right now there is a shoot
    function that goes on start, but further customizablity and such can be
    added pretty easily. (pointing in the right direction, ensuring
    velocities are additive, different ammo types, muzzle velocities, etc.)

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+
+	public GameObject ammo;
+
+	void Shoot(GameObject ammo) {
+		Debug.Log("shooting!");
+		GameObject shot = (GameObject)Instantiate(ammo, transform.position, Quaternion.identity);
+		Rigidbody shot_rb = shot.GetComponent<Rigidbody>();
+		shot_rb.AddForce(new Vector3(1000f, 0f));
+	}
+
+	void Start()
+	{
+		Shoot(ammo);
+	}
+}
